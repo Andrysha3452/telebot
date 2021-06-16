06d2a248375730a5c655937ae3576f26fdd8daf0
Andrysha3452 <70117145+Andrysha3452@users.noreply.github.com>
2021-06-16 16:52:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,12 @@
-import telebot 
+
+import config
+import telebot
+
 bot = telebot.TeleBot('1855568832:AAFxk0I2fT9EG4US1FfB_eeJ4odUs8udD-0')
 
-@bot.message_handler(content_types=['text'])
-def handle_text_messages(message):
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
 
-   if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет")
-    elif message.text == "Кто ты?":
-        bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера.")
-    elif message.text == "Как тебя зовут?":
-        bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
-    elif message.text == "Что ты умеешь?":
-        bot.send_message(message.from_user.id, "Я умею отвечать на несколько простых вопросов - кто я, как меня зовут и что я умею делать.")
-    else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши что-то другое.")
-
-bot.polling(none_stop=True, interval=0)
-
+if __name__ == '__main__':
+     bot.infinity_polling()
